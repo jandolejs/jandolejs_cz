3b9ede7f897de56db7bb5581a63bfcd52d6a9505
Jan Dolejš <jandolejs1999@gmail.com>
2022-06-08 14:39:50 +0200

    edit js

--- src/www/script.js
+++ src/www/script.js
@@ -1,4 +1,15 @@
 
-function copyTextToClipboard(text) {
+function copyTextToClipboard(text, e = null) {
     navigator.clipboard.writeText(text);
+
+    if (e && typeof e === 'object' && e.tagName === 'IMG') {
+        e = $(e);
+        e.fadeOut(500, function() {
+            e.attr("src","/media/copied.png").fadeIn(400, function () {
+                e.fadeOut(500, function() {
+                    e.attr("src", "/media/copy.png").fadeIn(500);
+                })
+            });
+        });
+    }
 }
