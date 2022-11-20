fc6b9eff5c0ea22ba44dba028549a92232c15b2b
Jan Dolejš <jandolejs1999@gmail.com>
2022-11-20 21:52:59 +0100

    improve table + delete note

--- src/app/Presenters/NotesPresenter.php
+++ src/app/Presenters/NotesPresenter.php
@@ -23,7 +23,32 @@ final class NotesPresenter extends Presenter
         $this->redirect('Notes:default');
     }
 
+    public function actionDelete() {
+        $hash = $this->getParameter('id');
+
+        if (empty($hash)) {
+            $this->flashMessage("ID of note is missing (/note/id/a1b2c3)", 'danger');
+            $this->redirect('Notes:default');
+        }
+
+        if (!$this->getUser()->isLoggedIn()) {
+            $this->flashMessage('You must be logged in', 'danger');
+            $this->redirect("Notes:default");
+        }
+
+        $note = $this->notesFactory->loadByHash($hash);
+
+        if ($note) {
+            $note->update(['visible' => 0]);
+            $this->flashMessage("Note '" . htmlspecialchars($hash) . "' was deleted", 'success');
+        } else {
+            $this->flashMessage("Note '" . htmlspecialchars($hash) . "' was not found", 'danger');
+        }
+        $this->redirect("Notes:default");
+    }
+
     public function actionNew() {
+
         $data = [
             'heading' => $this->getParameter('header'),
             'content' => $this->getParameter('content'),
@@ -31,6 +56,14 @@ final class NotesPresenter extends Presenter
             'public' => $this->getParameter('public') ? 1:0,
             'user_id' => $this->getUser()->getId(),
         ];
+
+        if ($hash = $this->getParameter('hash')) {
+            $exists = $this->notesFactory->loadByHash($hash);
+            if ($exists && $exists->id) {
+                $this->flashMessage('This hash already exists', 'warning');
+                $this->redirect('Notes:default', $data + ['modal' => '1', 'modalError' => 'Hash already exists']);
+            }
+        }
 
         $note = $this->notesFactory->getTable()->insert($data);
         $hash = $note->hash ?? $note->id;
@@ -45,5 +78,6 @@ final class NotesPresenter extends Presenter
         );
 
         $this->getTemplate()->notes = $notes;
+        $this->getTemplate()->request = $this->getRequest();
     }
 }
