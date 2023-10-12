e654453079ea1eca522eb7101722a90f7fd92b4a
Jan Dolejs <jandolejs1999@gmail.com>
2023-10-12 14:44:26 +0200

    add exit post

--- src/app/Presenters/BlogPresenter.php
+++ src/app/Presenters/BlogPresenter.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use App\Presenter;
+use JetBrains\PhpStorm\NoReturn;
+use Nette\Application\UI\Form;
 
 
 final class BlogPresenter extends Presenter
@@ -28,5 +30,60 @@ final class BlogPresenter extends Presenter
         }
 
         $this->template->post = $post;
+    }
+
+    public function renderEdit(int $id): void
+    {
+        if (!$this->getUser()->isLoggedIn()) {
+            $this->error('Please login to edit this post');
+        }
+
+        $post = $this->database
+            ->table('posts')
+            ->get($id);
+
+        if (!$post) {
+            $this->error('Post not found');
+        }
+
+        $this->getComponent('postForm')
+            ->setDefaults($post->toArray());
+    }
+
+
+    protected function createComponentPostForm(): Form
+    {
+        $form = new Form;
+        $form->addText('title', 'Title:')
+            ->setRequired();
+        $form->addText('heading', 'Heading:')
+            ->setRequired();
+        $form->addTextArea('content', 'Content:')
+            ->setRequired();
+
+        $form->addSubmit('send', 'Save and publish');
+        $form->onSuccess[] = $this->postFormSucceeded(...);
+
+        return $form;
+    }
+
+    #[NoReturn] private function postFormSucceeded(array $data): void
+    {
+        $postId = $this->getParameter('id');
+
+        if ($postId) {
+            $post = $this->database
+                ->table('posts')
+                ->get($postId);
+            $post->update($data);
+
+        } else {
+            $post = $this->database
+                ->table('posts')
+                ->insert($data);
+        }
+
+        $this->flashMessage('Post was published', 'success');
+        $this->redirect('Blog:post', $post->id);
     }
 }
