a0f88892f9119bb89794d7492918451333ef1a45
Jan Dolejs <jandolejs1999@gmail.com>
2024-01-02 22:34:37 +0000

    remove botstrap conf

--- src/app/Bootstrap.php
+++ src/app/Bootstrap.php
@@ -27,7 +27,6 @@ class Bootstrap
 			->register();
 
 		$configurator->addConfig($appDir . '/config/common.neon');
-		$configurator->addConfig($appDir . '/config/local.neon');
 
 		return $configurator;
 	}
